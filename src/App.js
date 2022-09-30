58b2f21069975e10412c4dbda2741e9b6a60f081
Dan ONeil <dhoneil@gmail.com>
2022-09-30 19:30:17 -0400

    Install Material UI, build theme prodiver & create theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import './App.css';
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Form from './components/Form';
 import Menu from './components/Menu';
+import './App.css';
 
 
 function App() {
-  return (
-    <div className="App">
-      <Menu/>
-      <Form/>
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#685c7f',
+      },
+      secondary: {
+        main: '#3d9991',
+      },
+      error: {
+        main: '#f44336',
+      },
+      success: {
+        main: '#4caf50',
+      },
+      warning: {
+        main: '#ff9800',
+      },
+      info: {
+        main: '#2196f3',
+      },
+    },
+  });
 
-    </div>
+  return (
+    <ThemeProvider theme={theme} >
+      <div className="App">
+        <Menu />
+        <Form />
+
+      </div>
+    </ThemeProvider>
   );
 }
 
